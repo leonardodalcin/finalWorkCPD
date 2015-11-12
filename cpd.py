6379b25cfc92f03edcaf08b6f5b7391ea7c539d0
leonardodalcin <leonardodalcin@outlook.com>
2015-11-12 14:32:59 -0200

    en, formated

--- cpd.py
+++ cpd.py
@@ -4,78 +4,91 @@ import random
 import os
 from pprint import pprint
 
+#main
 def main():
-	flagFIM = 0
-	print("iniciou o programa")
-	#ja existe database binaria?
+	endFlag = 0
+	print( "Start" )
+	
+#tests if db exists
 	try: 
-		with open('magicCards.bin','rb') as bindata:
-			binario = bindata.read();
-		print("achou o binario")
+		with open( 'magicCards.bin', 'rb' ) as bindata:
+			bin = bindata.read();
+		print( "Binary successfully loaded" )
+		
 	except:
-		#nao existe binario. criar novo
-		print("nao achou o binario")            
-		createDb() #le o .json, ordena, tira redundancias
-	while flagFIM == 0:
-		flagFIM = printMenu() #printa menu ate que retorne 1
+		print( "Opss, binary not found" )            
+		createDb()
+	
+	print( 'Welcome to the great Magic the Gathering DB' )
+	
+	while endFlag == 0:
+		endFlag = printMenu()
 
 #prints the menu and asks for a command
 def printMenu():
 	os.system('cls') #limpa a tela / clear screen
-	print( 'Welcome to the great Magic the Gathering DB' )
 	print( 'What are you looking for?' )
-	print('[0]exemplo \n[1]search name \n[2]procura por ID \n[3]Fechar')
+	print( '[0]Example \n[1]Search by name \n[2]Search by id \n[3]Quit' )
+	print( '>', end = "" )
 	c = int(input());
+	
+#prints example ------- Working
 	if c == 0:
-		#printa um exemplo de uma carta
-		printCardExample();
-		return 0 #main
+		card = cardExample();
+		pprint( card ) 
+		return 0
+		
+#search by name	------- Needs implementation
 	elif c == 1:
-		#pesquisa uma carta pelo ['name']
 		print( 'And what is the name of the beauty?' )
 		name = input();
-		searchName(name);       #precisara ser modificada pq
-					#vai procurar direto no binario
-		return 0 #main
+		card = searchName( name ); 
+		pprint( card )
+		return 0
+		
+#search by id -------- Needs a B-tree
 	elif c == 2:
-		print('Insira o ID da carta')
+		print( 'Please, insert card id' )
 		ID = input()
-		#searchID(ID) funcao a ser criada
-		return 0 #main
+		return 0
+		
+#ends process --------- Working
 	elif c == 3:
-		# fim do while do main
-		return 1 #flagFIM = 1 
+		return 1
+		
 	else:
-		print( 'please insert useful information' )
+		print( 'Please insert useful information' )
 	
-
+#creates db
 def createDb():
 	global db
 	db = []
-	with open( "AllCards.json") as data:
+	
+	with open( "magicCards.json" ) as data:
 		jsondb = json.load( data );
+		
 	if jsondb:
-		print('database .json loaded')
+		print( 'JSON database successfully loaded' )
 
+#appends cards to db and creates its id
 	i = 0
 	for name,card in jsondb.items():
-	       db.append(jsondb[name])
-	       db[i]['id'] = i
+	       db.append( jsondb[ name ])
+	       db[ i ][ 'id' ] = i
 	       i +=1
 	
-#search a card by name in the db and prints it
-def searchName( name ):
-        
+#searchs for a card by name and returns it
+def searchName( name ):  
 	for i in range( 0, len( db )):
-		if( db[ i ]['name'].lower() == name.lower()):
-			pprint( db[ i ] );
-			return
-
-def printCardExample():
-	#printa como exemplo uma carta de indice aleatorio
-	aCardIndex = random.randint( 0, len(db))
-	pprint(db[aCardIndex])
+		if( db[ i ][ 'name' ].lower() == name.lower()):
+			return( db[ i ] );
+	
+	return( 'Not found' )
+	
+#prints an example with random index
+def cardExample():
+	aCardIndex = random.randint( 0, len( db ))
+	pprint( db[ aCardIndex ])
 
 #initFunctions
 main();
-print('print depois do main')
